78d4b146f0351084b555ed785e5454c97caafa07
setung <4evajsh@gmail.com>
2022-01-04 02:11:31 +0900

    Initial commit

---
+++ src/test/kotlin/com/setung/freelec/springboot2/webservice/kotlin/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.setung.freelec.springboot2.webservice.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
